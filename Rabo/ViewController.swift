dd44a025be1b3b2def99ba2db6ad7dc5f94e659e
Kostya Gorbunov <kgorbunov@viber.com>
2019-10-22 16:54:49 +0200

    - initial commit based on default Single view xCode template;

---
+++ Rabo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Rabo
+//
+//  Created by Kostiantyn Gorbunov on 22/10/2019.
+//  Copyright © 2019 Kostiantyn Gorbunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
